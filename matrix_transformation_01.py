21bbad11561babaeb5caa7e7f2dfce006b91dd54
operationstratus <61917940+operationstratus@users.noreply.github.com>
2020-12-29 12:05:51 +0100

    Matrix Transformation

--- matrix_transformation_01.py
+++ matrix_transformation_01.py
@@ -1,124 +1,119 @@
 import tkinter as tk
 import math
 
-S = 800
-a = math.pi / 40
 
+# GLOBAL CONSTANTS
+S = 800
 U = 40
 
-BASE = [
-     [1, 2],
-     [2, 1]
-    ]
-print("BASE =")
-for row in BASE:
-    print(row)
-
-B = [
-     [0, -1],
-     [2, 0]
-    ]
-print("B =")
-for row in B:
-    print(row)
-
-
-def matrix_mult(A, B):
-    C = []
-    for row in A:
-        C.append([row[0]*B[0][0] + row[1]*B[1][0] , row[0]*B[0][1] + row[1]*B[1][1] ])
-    return C
-A = matrix_mult(B, BASE)
-print("A = ")
-for row in A:
-    print(row)
-
-
-'''
-A = [
-     [math.cos(a)*2, -math.sin(a)],
-     [math.sin(a), math.cos(a)]
-    ]
-'''
-'''
-A = [
-     [1, 0],
-     [0, 1]
-    ]
-'''
-
-root = tk.Tk()
-CANVAS = tk.Canvas(root, width=S, height=S, background="black")
+ROOT = tk.Tk()
+CANVAS = tk.Canvas(ROOT, width=S, height=S, background="black")
 CANVAS.pack(fill="both", expand=True)
-
-#CANVAS.create_oval(-5,-5,5,5, fill="red")
 CANVAS.configure(scrollregion=(-S/2, -S/2, S/2, S/2))
 CANVAS.xview_moveto(.5)
 CANVAS.yview_moveto(.5)
 
+I = [
+     [1, 0],
+     [0, 1]
+    ]
 
-def X(x, y):
-    return x*A[0][0]+y*A[0][1]
-def Y(x, y):
-    # OBS here are the solution to the problem with tkinter having positive y direction pointing downwards
-    return -x*A[1][0]-y*A[1][1]
-
-def create_coordinate_system():
-    for i in range(int(-S/2), int(S/2), int(U)):
-        if i == 0:
-            w = 3
-        else:
-            w = 1
-        mid = int(S/2)
-        # ORIGINAL GRID
-        CANVAS.create_line(mid, i, -mid, i, fill='gray', width=w) # x axis
-        CANVAS.create_line(i, -mid, i, mid, fill='gray', width=w)
-        # TRANSFORMED GRID
-        CANVAS.create_line(X(-mid, i), Y(-mid, i), X(mid, i), Y(mid, i), fill='white', width=w) # x axis
-        CANVAS.create_line(X(i, -mid), Y(i, -mid), X(i, mid), Y(i, mid), fill='white', width=w) # y axis
-
-'''
-def create_coordinate_system():
-    mid = int(S/2)
-    w = 1
-    CANVAS.create_line(-mid, 0, mid, 0, fill='white', width=w)
-    CANVAS.create_line(0, -mid, 0, mid, fill='white', width=w)
-'''
-def vec(x, y, color='yellow'):
-    print("vec: " + str(X(x, y)) + ", " + str(X(x, y)) )
-    x = x*U
-    y = y*U
-    CANVAS.create_line(0, 0, X(x, y), Y(x, y), fill=color, width=4)
-
-
-def show_standard_base_vectors():
-    vec(1, 0, 'green')
-    vec(0, 1, 'red')
-
-def rect(x1, y1, x2, y2, x3, y3, x4, y4):
-    x1 = x1*U
-    y1 = y1*U
-    x2 = x2*U
-    y2 = y2*U
-    x3 = x3*U
-    y3 = y3*U
-    x4 = x4*U
-    y4 = y4*U
-    points = [X(x1, y1), Y(x1, y1), X(x2, y2), Y(x2, y2), X(x3, y3), Y(x3, y3), X(x4, y4), Y(x4, y4)]
-    CANVAS.create_polygon(points, outline='#f11', fill='#1f1', width=2)
+class Base_system():
+    def __init__(self, base=I, trans_matrix=I):
+        self.__base = base
+        self.__trans_matrix = trans_matrix
+        self.__m_tot = matrix_mult(self.__base, self.__trans_matrix)
+            # WHAT?? It seems that I have to calculate the total matrix as base matrix * transformation matrix and not the other way around
+        self.__standard = self.__base == I
     
-def vec_in_standard_base(x, y, color="blue"):
-    x = x*U
-    y = -y*U
-    CANVAS.create_line(0, 0, x, y, fill=color, width=4)
+    def X_base(self, x, y):
+        return x*self.__base[0][0]+y*self.__base[0][1]
+    def Y_base(self, x, y):
+        # OBS here are the solution to the problem with tkinter having positive y direction pointing downwards
+        return -x*self.__base[1][0]-y*self.__base[1][1]
+    
+    def X(self, x, y):
+        return x*self.__m_tot[0][0]+y*self.__m_tot[0][1]
+    def Y(self, x, y):
+        # OBS here are the solution to the problem with tkinter having positive y direction pointing downwards
+        return -x*self.__m_tot[1][0]-y*self.__m_tot[1][1]
+    
+    def vec_base(self, x, y, color='yellow'):
+        x = x*U
+        y = y*U
+        CANVAS.create_line(0, 0, self.X_base(x, y), self.Y_base(x, y), fill=color, width=4)
+    
+    def vec(self, x, y, color='yellow'):
+        x = x*U
+        y = y*U
+        CANVAS.create_line(0, 0, self.X(x, y), self.Y(x, y), fill=color, width=4)
+    
+    def rect(self, x1, y1, x2, y2, x3, y3, x4, y4):
+        x1 = x1*U
+        y1 = y1*U
+        x2 = x2*U
+        y2 = y2*U
+        x3 = x3*U
+        y3 = y3*U
+        x4 = x4*U
+        y4 = y4*U
+        points = [self.X(x1, y1), self.Y(x1, y1), self.X(x2, y2), self.Y(x2, y2), self.X(x3, y3), self.Y(x3, y3), self.X(x4, y4), self.Y(x4, y4)]
+        CANVAS.create_polygon(points, outline='#f11', fill='#1f1', width=2)
+    
+    def base_coordinate_system(self):
+        for i in range(int(-S/2), int(S/2), int(U)):
+            if i == 0:
+                w = 3
+            else:
+                w = 1
+            mid = int(S/2)
+            
+            CANVAS.create_line(self.X_base(-mid, i), self.Y_base(-mid, i), self.X_base(mid, i), self.Y_base(mid, i), fill='gray', width=w) # x axis
+            CANVAS.create_line(self.X_base(i, -mid), self.Y_base(i, -mid), self.X_base(i, mid), self.Y_base(i, mid), fill='gray', width=w) # y axis
+            self.vec_base(1, 0, 'green')
+            self.vec_base(0, 1, 'red')
+    
+    def transformed_coordinate_system(self):
+        if self.__standard:
+            colour = 'gray'
+        else:
+            colour = 'white'
+        for i in range(int(-S/2), int(S/2), int(U)):
+            if i == 0:
+                w = 3
+            else:
+                w = 1
+            mid = int(S/2)
+            CANVAS.create_line(self.X(-mid, i), self.Y(-mid, i), self.X(mid, i), self.Y(mid, i), fill=colour, width=w) # x axis
+            CANVAS.create_line(self.X(i, -mid), self.Y(i, -mid), self.X(i, mid), self.Y(i, mid), fill=colour, width=w) # y axis
+        # base vectors
+        self.vec(1, 0, 'green')
+        self.vec(0, 1, 'red')
+
+
+
+def matrix_mult(a, b):
+    c = []
+    for row in a:
+        c.append([row[0]*b[0][0] + row[1]*b[1][0] , row[0]*b[0][1] + row[1]*b[1][1] ])
+    return c
+
 
 
 # MAIN
-create_coordinate_system()
-show_standard_base_vectors()
-vec(1, 1)
-#rect(0, 0, 0, 1, 1, 1, 1, 0)
-#vec(1, 0)
-#vec_in_standard_base(1, 1)
+standard = Base_system()
+standard.base_coordinate_system()
 
-root.mainloop()
+
+beta = Base_system([
+     [1, 2],
+     [2, 1]
+    ], [
+     [0, -1],
+     [2, 0]
+        ])
+beta.base_coordinate_system()
+beta.vec(1,1)
+
+
+ROOT.mainloop()
